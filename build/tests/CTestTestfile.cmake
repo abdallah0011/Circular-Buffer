eb5f11823962d42d11c9888bbbfd651b4c14bb32
Abdallah Osama <51860412+abdallah0011@users.noreply.github.com>
2024-05-22 21:13:34 +0300

    Delete build directory

--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/f/ubuntu/workspace/cplus/circularBuffer/tests
-# Build directory: /mnt/f/ubuntu/workspace/cplus/circularBuffer/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/mnt/f/ubuntu/workspace/cplus/circularBuffer/build/tests/circularBuffer_test[1]_include.cmake")
